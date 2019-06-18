f6b5de307a9bd78e3f2bd42c74f6a8de6a98ff53
student <sorush.kh68@gmail.com>
2019-06-18 18:10:44 +0100

    We can find the items in our application.

--- code/app.py
+++ code/app.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self,name):
-        return({'student': name})
+class Item(Resource):
 
-api.add_resource(Student, '/student/<string:name>')
+    def get(self,name):
+        items = {"book":1000}
+        for item,price in items.items():
+            if item== name:
+                return(jsonify({"name":item, "price":price}))
+        return(jsonify({"message":"item not found"}))
+
+
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5000)
